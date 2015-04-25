71151337e7c90c4893fc4b71e07a657ebf1fff0b
Eid010n <wpennell@gmail.com>
2015-04-25 17:59:22 -0400

    finished proxy handler function

--- Black-Hat-Python/proxy.py
+++ Black-Hat-Python/proxy.py
@@ -4,6 +4,21 @@ import sys
 import socket
 import threading
 
+# this is a pretty hex dumping function directly taken from
+# http://code.activestate.com/recipes/142812-hex-dumper/
+def hexdump(src, length=16):
+    result = []
+    digits = 4 if isinstance(src, unicode) else 2
+
+    for i in xrange(0, len(src), length):
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x))  for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.'  for x in s])
+        result.append( b"%04X   %-*s   %s" % (i, length*(digits + 1), hexa, text) )
+
+    print b'\n'.join(result)
+
+# main server loop for handling connections and forking a process to handler
 def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
     
     # create the server object
@@ -24,7 +39,7 @@ def server_loop(local_host,local_port,remote_host,remote_port,receive_first):
         client_socket, addr = server.accept()
         
         # print out the local connection information 
-        print"[+] Received incomming connections from %s:%d" % (addr[0],addr[1])
+        print"[+] Received incoming connections from %s:%d" % (addr[0],addr[1])
         
         # start a new thread to talk to the remote host
         proxy_thread = threading.Thread(target=proxy_handler,args=(client_socket,remote_host,remote_port,receive_first))
@@ -59,8 +74,8 @@ def proxy_handler(client_socket,remote_host,remote_port,receive_first):
             print "[*] Received %d bytes from localhost" % len(local_buffer)
             hexdump(local_buffer)
             
-            # send it to our handler
-            local_buffer = response_handler(local_buffer)
+            # send it to our request handler
+            local_buffer = request_handler(local_buffer)
             
             # send it off to the remote host
             remote_socket.send(local_buffer)
@@ -70,15 +85,34 @@ def proxy_handler(client_socket,remote_host,remote_port,receive_first):
         remote_buffer = received_from(remote_socket)
         
         if len(remote_buffer):
-            print "[*] Received %d bytes from remote." % len(remote)buffer
+            print "[*] Received %d bytes from remote." % len(remote_buffer)
+            hexdump(remote_buffer)
+            
+            # send it to our handler
+            remote_buffer = response_handler(remote_buffer)
+            
+            # sent it to the local host
+            client_socket.send(remote_buffer)
+            print "[*] Sent to local."
+            
+        # if nothing left to send, close down connections
+        if not len(remote_buffer) or not len(local_buffer):
+            remote_socket.close()
+            client_socket.close()
+            print "[*] No more data, closing connections/"       
+            break
     
 def receive_from(remote_socket):
     return
 
-def hexdump(remote_buffer):
+# handler function to modify server responses
+def response_handler(remote_buffer):
+    # perform packet modifications here
     return
 
-def response_handler(remote_buffer):
+# handler function to modify client requests
+def request_handler(local_buffer):
+    # perform packet modifications here
     return
         
 def main():
