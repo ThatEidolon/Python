8794b60efce7a49a5a8f126eeaf570fad6e87397
Eid010n <wpennell@gmail.com>
2015-04-26 21:48:23 -0400

    sshcmd.py
    still needs to be debugged

--- Black-Hat-Python/bh-sshcmd.py
+++ Black-Hat-Python/bh-sshcmd.py
@@ -3,3 +3,17 @@
 import threading
 import paramiko
 import subprocess
+
+def ssh_command(ip, user, passwd, command):
+    client = paramike.SSHClient()
+    ## client can also support using key files
+    #client.load_host_keys('/home/user/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print ssh_session.recv(1024)
+    return
+
+ssh_command('127.0.0.1', 'username', 'password', 'id')
