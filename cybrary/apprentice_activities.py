71a56b6800fefa8808221048c98416319cc528a5
Eidolon <Eid010n@users.noreply.github.com>
2015-04-28 13:04:55 -0400

    Create apprentice_activities.py

---
+++ cybrary/apprentice_activities.py
@@ -0,0 +1,16 @@
+def activity01(num1):
+	'''Determine if an input number is Even or Odd'''
+    return
+		
+def activity02(iv_one, iv_two):
+	'''Return the sum of two input values'''
+	return
+
+def activity03(num_list):
+	'''Given a list of integers, count how many are even'''
+	return
+	
+def activity04(input_string):
+	'''Return the input string, backward'''
+	return
+	
