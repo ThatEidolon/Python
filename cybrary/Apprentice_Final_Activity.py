07ed656b8db50f7321b43cc83df11f4165364b29
Eidolon <Eid010n@users.noreply.github.com>
2015-04-28 13:07:31 -0400

    Create Apprentice_Final_Activity.py

---
+++ cybrary/Apprentice_Final_Activity.py
@@ -0,0 +1,23 @@
+import operator
+
+saved_string = ''
+
+def remove_letter(): #Remove a selected letter from a string
+    return
+
+def num_compare(): #Compare 2 numbers to determine the larger
+    return
+
+def print_string(): #Print the previously stored string
+    return
+
+def calculator(): #Basic Calculator (addition, subtraction, multiplication, division)
+    return
+
+def accept_and_store(): #Accept and store a string
+    return
+
+def main(): #menu goes here
+    return
+
+main()
