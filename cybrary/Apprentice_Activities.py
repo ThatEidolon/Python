bb04aff522b9249e9d48cb8fd1f90e9fb591aad8
Eid010n <wpennell@gmail.com>
2015-04-28 20:16:26 -0400

    completed Apprentice Activities

--- cybrary/Apprentice_Activities.py
+++ cybrary/Apprentice_Activities.py
@@ -1,16 +1,35 @@
 def activity01(num1):
 	'''Determine if an input number is Even or Odd'''
-    return
+	try:
+		if int(num1) % 2:
+			return "Odd"
+		else:
+			return "Even"
+	except:
+		return "Not a number"
 		
 def activity02(iv_one, iv_two):
 	'''Return the sum of two input values'''
-	return
+	try:		
+		return int(iv_one) + int(iv_two)
+	except:
+		return "Does not contain only numbers"
 
 def activity03(num_list):
 	'''Given a list of integers, count how many are even'''
-	return
+	count = 0
+	try:
+		for i in num_list:
+			if not (int(i) % 2):
+				count += 1
+		return count
+	except:
+		return "List contains non-numbers"
+				
 	
 def activity04(input_string):
 	'''Return the input string, backward'''
-	return
-	
+	try:
+		return input_string[::-1]
+	except:
+		return "Not a string"
