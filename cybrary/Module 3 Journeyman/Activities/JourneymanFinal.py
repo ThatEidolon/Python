18bc76386d45ff24f13f7be32455d9b2c9a6a676
Eid010n <wpennell@gmail.com>
2015-05-12 15:01:39 -0400

    Building Skeleton

--- cybrary/Module 3 Journeyman/Activities/JourneymanFinal.py
+++ cybrary/Module 3 Journeyman/Activities/JourneymanFinal.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 '''
 1.5) Python Journeyman: Write a Python server which:
 	receives a connection from the included client (JourneymanFinal.py)
@@ -8,19 +10,56 @@ import socket
 '''
 
 def configure_server():
+    # configure server to listen on localhost port 50002, and listen for up to five clients
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('localhost', 50001))
+    s.bind(('localhost', 50002))
+    s.listen(5)
     return s
 
-def receive_data(some_socket):
+def server_loop():
+    server_socket = configure_server()
+    while True:
+        conn, addr = server_socket.accept()
+        client_thread = threading.Thread(target=client_handler, args=(conn,))
+        # setting daemon to true is required to terminated with ctrl-c
+        client_thread.daemon = True
+        client_thread.start()
+    return
+
+def save_data(filename, data):
+    # save data to filename
+    return
+
+def read_data(filename):
+    # read from filename and return data
     data = ''
     return data
 
-def send_data(some_socket, data):
+def client_handler(client_socket):
+    data = client_socket.recv(4096)    
+    mode = str(data[:4])
+    file_name = str(data[4:9]) # the fuck?!
+    contents = str(data[9:])
+    print "Mode: %s" % mode
+    print "File Name: %s" % file_name
+    print "Contents: %s" % contents
+    client_socket.close()
     return
 
+def receive_data(some_socket):
+    data = ''
+    data = some_socket.recv(4096)
+    return data
+
+def send_data(some_socket, data):
+    return some_socket
 
 def main():
+    # this is quick and dirty and will be a constant loop
+    
+    # configure a new server and save it as server socket
+    server_loop()
+    return
 
-
-main()
+if __name__ == "__main__":
+    main()
