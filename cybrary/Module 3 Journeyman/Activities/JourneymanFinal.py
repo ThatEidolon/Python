fcea97d136f2490c256ab72efbb619da5403fe6a
Eid010n <wpennell@gmail.com>
2015-05-16 16:12:51 -0400

    tweaking

--- cybrary/Module 3 Journeyman/Activities/JourneymanFinal.py
+++ cybrary/Module 3 Journeyman/Activities/JourneymanFinal.py
@@ -13,12 +13,14 @@ OPTIONS = ['SAVE', 'LOAD']
 	deals with errors and missing files
 '''
 
+
 def configure_server():
     # configure server to listen on localhost port 50002, and listen for up to five clients
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(5)
     return s
+
 
 def server_loop():
     server_socket = configure_server()
@@ -30,10 +32,12 @@ def server_loop():
         client_thread.start()
     return
 
+
 def save_data(filename, data):
     # save data to filename
     with open(filename, 'w+') as f:
         f.write(data)
+
 
 def send_file(some_socket, filename):
     # read from filename and return data
@@ -44,15 +48,14 @@ def send_file(some_socket, filename):
         some_socket.send("File Not Found")
 
 
-
 def client_handler(client_socket):
-    data = client_socket.recv(4096)    
+    data = client_socket.recv(4096)
     mode = str(data[:4])
-    file_name = str(data[4:9]) # the fuck?!
+    file_name = str(data[4:9])  # the fuck?!
     contents = str(data[9:])
-    if mode == OPTIONS[0]: # save
+    if mode == OPTIONS[0]:  # save
         save_data(file_name, contents)
-    elif mode == OPTIONS[1]: # load
+    elif mode == OPTIONS[1]:  # load
         send_file(client_socket, file_name)
     client_socket.close()
     return
@@ -60,10 +63,10 @@ def client_handler(client_socket):
 
 def main():
     # this is quick and dirty and will be a constant loop
-    
     # configure a new server and save it as server socket
     server_loop()
     return
+
 
 if __name__ == "__main__":
     main()
